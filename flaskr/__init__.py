8eb9dcc0f73d9bf2cb42d3fa2ec0927679cafada
edmilsonbraga <d1000so@unicamp.br>
2023-12-04 22:09:06 -0300

    commit app e db

---
+++ flaskr/__init__.py
@@ -0,0 +1,32 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # criar e configurar o aplicativo
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        #Carregar a configuração da instância, se ela existir, quando não estiver em fase de teste
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Carregar a configuração de teste se for passada
+        app.config.from_mapping(test_config)
+
+    # garantir que a pasta da instância exista
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # uma página simples que diz "olá"
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
